3e315621a2eed3314119d59a4c11e4f24b2cbdd7
iOSDevLime <onthelots@naver.com>
2023-07-31 13:25:38 +0900

    [Fix] ClientID, Secret, Redirect_url 빈 문자열로 수정

--- Spotify_App/Spotify_App/ClientID/ClientID+Secret.swift
+++ Spotify_App/Spotify_App/ClientID/ClientID+Secret.swift
@@ -8,12 +8,12 @@
 import Foundation
 
 struct ClientID {
-    /// 🔒 How to get ClientID & ClientSecret Code
-    /// 1. Go to your Spotify for Developers Dashboard
-    /// 2. Click Your Create App  ( If you do not log in or create an app, please refer to README(https://github.com/onthelots/Spotify_App) )
-    /// 3. on the Basic Information tab
-    /// 4. Copy the [Client ID] and [Client Secret], [Redirect_url] code and write it in the string below (youtClientID, yourClientSecret, yourRedirectURL)
-    /// 5. After the simulation build, log in with an account subscribed to 'Spotify Development' (☑️ Must be the same account)
+    /// 🔒 앱 구동을 위한 준비(How to get ClientID & ClientSecret Code)
+    /// 1. 스포티파이 개발자 대시보드로 이동해주세요(Go to your Spotify for Developers Dashboard)
+    /// 2. 앱 등록을 진행해주세요(Click Your Create App)  ( If you do not log in or create an app, please refer to README(https://github.com/onthelots/Spotify_App) )
+    /// 3. Basic Information 탭을 클릭합니다(On the Basic Information tab)
+    /// 4. CliendID, ClientSecret, Redirect_url을 복사한 후, 아래 문자열에 입력해주세요 (Copy the [Client ID] and [Client Secret], [Redirect_url] code and write it in the string below (youtClientID, yourClientSecret, yourRedirectURL))
+    /// 5. 시뮬레이션을 빌듣한 후, 본인의 스포티파이 아이디로 로그인해주세요(After the simulation build, log in with an account subscribed to 'Spotify Development' (☑️ Must be the same account))
 
     static let youtClientID: String = ""
     static let yourClientSecret: String = ""
